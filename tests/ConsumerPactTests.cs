bbaa45c03b34b16828935886c723911ddef14d65
PrakharRoy <44992336+PrakharRoy@users.noreply.github.com>
2023-06-09 07:50:30 -0400

    Update ConsumerPactTests.cs
    Added a test case to get product by ID

--- tests/ConsumerPactTests.cs
+++ tests/ConsumerPactTests.cs
@@ -27,7 +27,9 @@ namespace tests
 
             products = new List<object>()
             {
-                new { id = "27", name = "burger", type = "food" }
+                new { id = "27", name = "burger", type = "food" },
+                new { id = "28", name = "pizza", type = "food"},
+                new { id = "29", name = "pasta", type = "food"}
             };
 
             var Config = new PactConfig
@@ -62,6 +64,32 @@ namespace tests
                 result.Should().HaveCount(1);
                 Assert.Equal("27",result[0].id);
                 Assert.Equal("burger",result[0].name);
+                Assert.Equal("food",result[0].type);
+            });
+        }
+        
+        [Fact]
+        public async Task ConsumerPactTests_RetrieveProducts_RetieveAProduct2()
+        {
+            // Arrange
+            pact.UponReceiving("A request to get product by Id")
+                        .Given("products exist")
+                        .WithRequest(HttpMethod.Get, "/products")
+                    .WillRespond()
+                    .WithStatus(HttpStatusCode.OK)
+                    .WithHeader("Content-Type", "application/json; charset=utf-8")
+                    .WithJsonBody(Match.MinType(products[1], 1));
+
+            await pact.VerifyAsync(async ctx =>
+            {
+                // Act
+                var consumer = new ProductClient();
+                List<Product> result = await consumer.GetProducts(ctx.MockServerUri.ToString().TrimEnd('/'));
+                // Assert
+                result.Should().NotBeNull();
+                result.Should().HaveCount(1);
+                Assert.Equal("28",result[0].id);
+                Assert.Equal("pizza",result[0].name);
                 Assert.Equal("food",result[0].type);
             });
         }
